eb41165d49d21878670e3febdb21c107a7a64933
ASaf <safieddineahmad92@gmail.com>
2026-06-11 23:20:59 +0300

    chore: add copy-webpack-plugin, configure it, and update logo paths

--- wp-content/themes/eurofert-theme/webpack.config.js
+++ wp-content/themes/eurofert-theme/webpack.config.js
@@ -1,9 +1,16 @@
 const defaultConfig = require('@wordpress/scripts/config/webpack.config');
 const RemoveEmptyScriptsPlugin = require('webpack-remove-empty-scripts');
+const CopyWebpackPlugin = require('copy-webpack-plugin'); // Note: Used to ensure all theme images are copied to public/images
+
 module.exports = {
   ...defaultConfig,
   plugins: [
     ...defaultConfig.plugins,
-    new RemoveEmptyScriptsPlugin()
+    new RemoveEmptyScriptsPlugin(),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: 'resources/images', to: 'images', noErrorOnMissing: true }
+      ]
+    })
   ]
 };
